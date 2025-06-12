cd0c8af7eaa33eab084fa1698e2c946eb7be3b03
Muhammad-Usman-RBS <mtlcontrol8@gmail.com>
2025-06-12 17:31:46 +0500

    Update 2

--- backend/utils/createSuperAdmin.js
+++ backend/utils/createSuperAdmin.js
@@ -1,36 +1,9 @@
-// import User from '../models/User.js';
-// import bcrypt from 'bcryptjs';
-
-// // Function to create a default super admin if it doesn't already exist
-// const createSuperAdmin = async () => {
-//   const email = process.env.superAdminEmail; // Hardcoded super admin email
-//   const password = process.env.superAdminPsw; // Hardcoded password (should be kept secure or moved to .env)
-
-//   const existing = await User.findOne({ email });
-
-//   if (!existing) {
-//     const hashed = await bcrypt.hash(password, 10); // Hash the password using bcrypt
-//     await User.create({
-//       email,
-//       password: hashed,
-//       role: 'superadmin', // Assign 'superadmin' role
-//     });
-//     console.log('Super Admin Created'); // Confirmation message
-//   }
-// };
-
-// export default createSuperAdmin;
-
-
-
-
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
 
-// Function to create a default super admin if it doesn't already exist
 const createSuperAdmin = async () => {
-  const email = process.env.superAdminEmail; // Hardcoded super admin email
-  const password = process.env.superAdminPsw; // Hardcoded password (should be kept secure or moved to .env)
+  const email = process.env.SUPERADMINEMAIL; 
+  const password = process.env.SUPERADMINPSW; 
 
   const existing = await User.findOne({ email });
   const allowedPermissions = [
@@ -40,7 +13,7 @@ const createSuperAdmin = async () => {
 ];
 
   if (!existing) {
-    const hashed = await bcrypt.hash(password, 10); // Hash the password using bcrypt
+    const hashed = await bcrypt.hash(password, 10);
     await User.create({
       email,
       password: hashed,
