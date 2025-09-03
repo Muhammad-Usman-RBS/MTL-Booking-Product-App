1eb87704191fea33b07577629be2b9029e252776
Mashhood-IT <mltcontrol10@gmail.com>
2025-09-03 18:59:19 +0500

    Driver-format

--- frontend/src/components/dashbaord/drivers/DriverList.jsx
+++ frontend/src/components/dashbaord/drivers/DriverList.jsx
@@ -20,7 +20,7 @@ const tabOptions = ["Active", "Suspended", "Pending", "Deleted"];
 const DriverList = () => {
   const user = useSelector((state) => state?.auth?.user);
   const companyId = user?.companyId;
-  
+
   const [activeTab, setActiveTab] = useState("Active");
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
@@ -37,13 +37,13 @@ const DriverList = () => {
   const [deleteDriverById] = useDeleteDriverByIdMutation();
   const driversArray = getAllDrivers?.drivers || [];
   const roleFilteredDrivers =
-  user?.role === "driver"
-    ? driversArray.filter(
-        (driver) =>
-          driver?.DriverData?.employeeNumber?.toString() ===
-          user?.employeeNumber?.toString()
-      )
-    : driversArray;
+    user?.role === "driver"
+      ? driversArray.filter(
+          (driver) =>
+            driver?.DriverData?.employeeNumber?.toString() ===
+            user?.employeeNumber?.toString()
+        )
+      : driversArray;
 
   const filteredTabData = roleFilteredDrivers.filter(
     (driver) => driver?.DriverData?.status === activeTab
@@ -112,46 +112,46 @@ const DriverList = () => {
   }));
 
   const tableData = !filteredData.length
-  ? EmptyTableMessage({
-      message: "No drivers found. Create a new driver.",
-      colSpan: tableHeaders.length,
-    })
-  : paginatedData.map((driver, index) => ({
-    index:
-      (page - 1) * (perPage === "All" ? filteredData.length : perPage) +
-      index +
-      1,
-    employeeNumber: driver?.DriverData?.employeeNumber,
-    firstName: driver?.DriverData?.firstName,
-    email: driver?.DriverData?.email,
-    carMake: driver?.VehicleData?.carMake,
-    carModal: driver?.VehicleData?.carModal,
-    status: driver?.DriverData?.status,
-    actions: (
-      <div className="flex gap-2">
-        <Icons.Eye
-          className="w-8 h-8 rounded-md hover:bg-blue-600 hover:text-white text-[var(--dark-gray)] cursor-pointer border border-[var(--light-gray)] p-2"
-          onClick={() => setSelectedDriver(driver._id)}
-        />
-        <Link to={`/dashboard/drivers/edit/${driver._id}`}>
-          <Icons.Pencil className="w-8 h-8 rounded-md hover:bg-yellow-600 hover:text-white text-[var(--dark-gray)] cursor-pointer border border-[var(--light-gray)] p-2" />
-        </Link>
-        <Icons.Send
-          className="w-8 h-8 rounded-md hover:bg-blue-500 hover:text-white text-[var(--dark-gray)] cursor-pointer border border-[var(--light-gray)] p-2"
-          onClick={() => handleSendEmail(driver)}
-        />
-        {user?.role !== "driver" && (
-          <Icons.X
-            onClick={() => {
-              setdriverToDelete(driver);
-              setShowDeleteModal(true);
-            }}
-            className="w-8 h-8 rounded-md hover:bg-red-800 hover:text-white text-[var(--dark-gray)] cursor-pointer border border-[var(--light-gray)] p-2"
-          />
-        )}
-      </div>
-    ),
-  }));
+    ? EmptyTableMessage({
+        message: "No drivers found. Create a new driver.",
+        colSpan: tableHeaders.length,
+      })
+    : paginatedData.map((driver, index) => ({
+        index:
+          (page - 1) * (perPage === "All" ? filteredData.length : perPage) +
+          index +
+          1,
+        employeeNumber: driver?.DriverData?.employeeNumber,
+        firstName: driver?.DriverData?.firstName,
+        email: driver?.DriverData?.email,
+        carMake: driver?.VehicleData?.carMake,
+        carModal: driver?.VehicleData?.carModal,
+        status: driver?.DriverData?.status,
+        actions: (
+          <div className="flex gap-2">
+            <Icons.Eye
+              className="w-8 h-8 rounded-md hover:bg-blue-600 hover:text-white text-[var(--dark-gray)] cursor-pointer border border-[var(--light-gray)] p-2"
+              onClick={() => setSelectedDriver(driver._id)}
+            />
+            <Link to={`/dashboard/drivers/edit/${driver._id}`}>
+              <Icons.Pencil className="w-8 h-8 rounded-md hover:bg-yellow-600 hover:text-white text-[var(--dark-gray)] cursor-pointer border border-[var(--light-gray)] p-2" />
+            </Link>
+            <Icons.Send
+              className="w-8 h-8 rounded-md hover:bg-blue-500 hover:text-white text-[var(--dark-gray)] cursor-pointer border border-[var(--light-gray)] p-2"
+              onClick={() => handleSendEmail(driver)}
+            />
+            {user?.role !== "driver" && (
+              <Icons.X
+                onClick={() => {
+                  setdriverToDelete(driver);
+                  setShowDeleteModal(true);
+                }}
+                className="w-8 h-8 rounded-md hover:bg-red-800 hover:text-white text-[var(--dark-gray)] cursor-pointer border border-[var(--light-gray)] p-2"
+              />
+            )}
+          </div>
+        ),
+      }));
 
   return (
     <>
@@ -159,39 +159,39 @@ const DriverList = () => {
         <OutletHeading name="Driver List" />
         {user?.role !== "driver" && (
           <>
-        <div className="flex flex-col sm:flex-row justify-between gap-4 px-4 sm:px-0 mb-4">
-          <Link to="/dashboard/drivers/new" className="w-full sm:w-auto">
-            <button className="btn btn-reset flex items-center gap-2 w-full sm:w-auto justify-center">
-              Create New Driver
-            </button>
-          </Link>
-        </div>
-      
-  <div className="w-full overflow-x-auto mb-4">
-    <div className="flex gap-4 text-sm font-medium border-b min-w-max sm:text-base px-2">
-      {tabOptions.map((tab) => (
-        <button
-          key={tab}
-          onClick={() => setActiveTab(tab)}
-          className={`pb-2 whitespace-nowrap transition-all duration-200 ${
-            activeTab === tab
-              ? "border-b-2 border-blue-600 text-blue-600"
-              : "text-[var(--dark-gray)] hover:text-blue-500"
-          }`}
-        >
-          {tab} (
-            {
-              roleFilteredDrivers.filter((d) => d?.DriverData?.status === tab).length
-            }
-          )
-        </button>
-      ))}
-    </div>
-  </div>
-  </>
+            <div className="flex flex-col sm:flex-row justify-between gap-4 px-4 sm:px-0 mb-4">
+              <Link to="/dashboard/drivers/new" className="w-full sm:w-auto">
+                <button className="btn btn-reset flex items-center gap-2 w-full sm:w-auto justify-center">
+                  Create New Driver
+                </button>
+              </Link>
+            </div>
 
-)}
-
+            <div className="w-full overflow-x-auto mb-4">
+              <div className="flex gap-4 text-sm font-medium border-b min-w-max sm:text-base px-2">
+                {tabOptions.map((tab) => (
+                  <button
+                    key={tab}
+                    onClick={() => setActiveTab(tab)}
+                    className={`pb-2 whitespace-nowrap transition-all duration-200 ${
+                      activeTab === tab
+                        ? "border-b-2 border-blue-600 text-blue-600"
+                        : "text-[var(--dark-gray)] hover:text-blue-500"
+                    }`}
+                  >
+                    {tab} (
+                    {
+                      roleFilteredDrivers.filter(
+                        (d) => d?.DriverData?.status === tab
+                      ).length
+                    }
+                    )
+                  </button>
+                ))}
+              </div>
+            </div>
+          </>
+        )}
 
         <CustomTable
           tableHeaders={tableHeaders}
